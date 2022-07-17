48a11a86526f53af598a42fdb910d3e214a750c9
Terry Babyuk <tbabyuk@gmail.com>
2022-07-17 00:57:42 -0400

    improved layout more, added contact buttons to top of page

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 
-const x = window.innerWidth;
 
-console.log(x)
+console.log("hello world")
+
+
+const contactLinks = document.querySelectorAll(".contact-link");
+const footer = document.getElementById("footer");
+const formInput = document.getElementById("name");
+
+
+
+// contactLink.addEventListener("click", () => {
+//     footer.scrollIntoView();
+//     console.log('done')
+//     formInput.focus();
+// })
+
+contactLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        footer.scrollIntoView();
+        console.log('done')
+        formInput.focus();
+    })
+})
